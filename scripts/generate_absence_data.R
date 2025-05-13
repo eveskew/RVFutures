8e8aa13d37b994ea4f8f9f84a627df193cad9765
Evan Eskew <eveskew@gmail.com>
2025-05-12 21:50:45 -0400

    Improvements to pseudo-absence code

--- scripts/generate_absence_data.R
+++ scripts/generate_absence_data.R
@@ -2,6 +2,7 @@ library(tidyverse)
 library(rnaturalearth)
 library(sf)
 library(terra)
+library(assertthat)
 
 sf_use_s2(FALSE)
 
@@ -9,7 +10,7 @@ source("R/functions.R")
 
 
 # Generate "absence" data (RVF outbreak background/pseudo-absence data) for use 
-# in modeling exercises
+# in modeling
 
 #==============================================================================
 
@@ -17,16 +18,20 @@ source("R/functions.R")
 # Import background outlines of Kenya, Tanzania, and Uganda
 east.africa <- load_country_map()
 
-# Create a masked version of the background outlines that erases out large
-# lakes so pseudo-absences don't end up showing up over water where we don't 
-# have predictor data
-lakes.10 <- readRDS("data/rasters/hydrology/saved_objects/lakes_east_africa_10.rds")
+# Create a masked version of the background outlines that erases out 
+# medium/large lakes so pseudo-absences don't end up showing up over water 
+# where we don't have predictor data
+lakes.5 <- readRDS("data/rasters/hydrology/saved_objects/lakes_east_africa_5.rds")
 st_erase <- function(x, y) st_difference(x, st_union(st_combine(y)))
-east.africa.lake.erase <- st_erase(east.africa, lakes.10)
+east.africa.lake.erase <- st_erase(east.africa, lakes.5)
 
 # Import observed RVF outbreak data
 d <- read_csv("data/outbreak_data/EC_RVF_clean_April27.csv")
 d.sf <- st_as_sf(d, coords = c("GPS_x", "GPS_y"), crs = st_crs(east.africa))
+
+n.points.per.yrmon <- 150
+# Define the outbreak buffer radius in degrees
+outbreak.buffer.radius <- 0.2
 
 #==============================================================================
 
@@ -34,69 +39,63 @@ d.sf <- st_as_sf(d, coords = c("GPS_x", "GPS_y"), crs = st_crs(east.africa))
 # Generate pseudo-absence points randomly from within the East African
 # country extent
 
-# More, specifically generate random pseudo-absence points for each year
+# More specifically, generate random pseudo-absence points for each month
 # in the observed data to completely evaluate the available
 # environmental space
 
 
-# Import a reference raster and build a grid for the study region
-r <- rast("data/rasters/elevation/processed/elevation_2.5min.tif")
-
-# Make a 10 arcminute grid across the study region
-east.africa.grid <- st_make_grid(
-  east.africa, 
-  cellsize = 10/60
-)
-
-n.points.per.yrmon <- 150
-points <- data.frame()
+random.points <- data.frame()
 
 set.seed(8)
 
 for(year in 2008:2022) {
   
-  # Calculate which cells in the grid were positive for actual RVF outbreaks
-  # in the focal year
-  outbreak.cells <- st_intersects(
-    filter(d.sf, OB_Yr == year),
-    east.africa.grid
-  ) %>%
-    unlist() %>%
-    unique() %>%
-    sort()
+  # Generate a buffer representing RVF outbreak locations in the focal year
+  outbreak.buffer <- d.sf %>%
+    filter(OB_Yr == year) %>%
+    st_buffer(dist = outbreak.buffer.radius * 2) %>%
+    st_union()
   
-  print(outbreak.cells)
-  
-  # Generate a map of East Africa with gaps for the positive grid cells to
-  # ensure that pseudo-absence data are not drawn from locations with actual
-  # RVF outbreaks
-  ifelse(
-    is.null(outbreak.cells),
-    sampling.area.for.year <- east.africa.lake.erase,
-    sampling.area.for.year <- st_erase(east.africa.lake.erase, east.africa.grid[outbreak.cells])
-  )
-  
-  # Generate a data frame of random pseudo-absence points across all months
-  # of the focal year
+  # Generate a data frame of random pseudo-absence points for the focal year, 
+  # drawing 5x as many points as ultimately needed to 
+  # account for later filtering based on known outbreak locations
   temp <- st_sample(
-    sampling.area.for.year, 
-    size = n.points.per.yrmon * 12
+    east.africa.lake.erase,
+    size = n.points.per.yrmon * 12 * 5
   ) %>%
     st_coordinates() %>%
     as.data.frame() %>%
     rename(
       GPS_x = X,
       GPS_y = Y
-    ) %>%
-    mutate(
-      OB_Yr = rep(year, times = n.points.per.yrmon * 12),
-      OB_Mo = rep(month.name, each = n.points.per.yrmon) 
     )
   
-  points <- bind_rows(points, temp)
+  # Filter points to eliminate any within the outbreak buffer zone,
+  # assuming there are outbreak points in that year
+  if (length(outbreak.buffer) > 0) {
+    
+    temp <- st_difference(
+      st_as_sf(temp, coords = c("GPS_x", "GPS_y"), crs = st_crs(east.africa)),
+      outbreak.buffer
+    ) %>%
+      mutate(
+        GPS_x = st_coordinates(.)[,1],
+        GPS_y = st_coordinates(.)[,2]
+      ) %>%
+      st_drop_geometry()
+  }
+  
+  assert_that(nrow(temp) >= n.points.per.yrmon * 12)
+  
+  # Subsample down to only the required number of points for a year
+  temp <- slice_sample(temp, n = n.points.per.yrmon * 12) %>%
+    mutate(
+      OB_Yr = rep(year, times = n.points.per.yrmon * 12),
+      OB_Mo = rep(month.name, each = n.points.per.yrmon)
+    )
+  
+  random.points <- bind_rows(random.points, temp)
 }
-
-random.points <- points
 
 #==============================================================================
 
@@ -140,9 +139,9 @@ ggplot() +
 
 n.points.per.obs <- 10
 
-set.seed(8)
+doughnut.points <- data.frame()
 
-points <- data.frame()
+set.seed(8)
 
 for(i in 1:nrow(d.sf)) {
   
@@ -161,10 +160,8 @@ for(i in 1:nrow(d.sf)) {
       OB_Mo = rep(d$OB_Mo[i], times = n.points.per.obs) 
     )
   
-  points <- bind_rows(points, temp)
+  doughnut.points <- bind_rows(doughnut.points, temp)
 }
-
-doughnut.points <- points
 
 #==============================================================================
 
@@ -185,22 +182,15 @@ r <- terra::rast(files)
 
 # Calculate total population size represented in the raster and plot
 cs <- cellSize(r, unit = "km")
-global(r * cs, "sum", na.rm = T)
+global(r * cs, "sum", na.rm = TRUE)
 plot(r)
 
-# Aggregate the raster to 10 arcminute grid cells and report prior statistics
-r.agg <- aggregate(r, fact = 4, fun = "mean")
-cs.agg <- cellSize(r.agg, unit = "km")
-global(r.agg * cs.agg, "sum", na.rm = T)
-plot(r.agg)
-
-
 # Make the East Africa map into a grid matching the extent and resolution of 
-# the aggregated raster
+# the raster
 east.africa.grid <- st_make_grid(
   east.africa, 
-  n = c(dim(r.agg)[2], dim(r.agg)[1]),
-  cellsize = res(r.agg)
+  n = c(dim(r)[2], dim(r)[1]),
+  cellsize = res(r)
 )
 
 ggplot() + 
@@ -212,30 +202,21 @@ ggplot() +
 # Generate pseudo-absence points in proportion to the human population density
 # raster
 
-n.points.per.yrmon <- 150
 human.points <- data.frame()
+
+set.seed(8)
 
 for(year in 2008:2022) {
   
-  # Pull the correct population raster from the aggregated raster stack
+  # Generate a buffer representing RVF outbreak locations in the focal year
+  outbreak.buffer <- d.sf %>%
+    filter(OB_Yr == year) %>%
+    st_buffer(dist = outbreak.buffer.radius * 2) %>%
+    st_union()
+  
+  # Pull the correct population raster from the raster stack
   year.mod <- ifelse(year > 2020, 2020, year)
-  r.temp <- r.agg[[str_detect(names(r.agg), as.character(year.mod))]]
-  
-  # Modify the aggregated human population density raster just to reduce the
-  # magnitude of the values and therefore generate a reasonable number of 
-  # pseudo-absence points
-  r.temp <- r.temp/15
-  
-  # Generate a vector of cells for this year with RVF outbreak cases
-  outbreak.cells <- st_intersects(
-    filter(d.sf, OB_Yr == year),
-    east.africa.grid
-  ) %>%
-    unlist() %>%
-    unique() %>%
-    sort()
-  
-  print(outbreak.cells)
+  r.temp <- r[[str_detect(names(r), as.character(year.mod))]]
   
   # Generate a vector of human population density values
   # (corresponding to the value of each raster grid cell) 
@@ -246,43 +227,57 @@ for(year in 2008:2022) {
     flip(direction = "vertical") %>%
     # extract values
     values() %>%
-    # convert population density values to integers
-    as.integer() %>%
     # convert NA values to zeroes
     ifelse(is.na(.), 0, .)
   
-  # Set outbreak cell values to 0 so they will not be sampled for 
-  # pseudo-absences
-  human.pop.vec[outbreak.cells] <- 0
+  # With what probability should each grid cell be sampled?
+  probs <- human.pop.vec / sum(human.pop.vec)
+  # Sample grid cells in proportion to their probability
+  samples <- sample(1:length(human.pop.vec), size = 10000, prob = probs, replace = TRUE)
+  # Generate a vector giving the point count needed from each grid cell
+  counts <- table(factor(samples, levels = 1:length(human.pop.vec)))
   
-  # How many pseudo-absences will be generated?
-  print(sum(human.pop.vec))
+  assert_that(length(east.africa.grid) == length(counts))
   
-  assertthat::assert_that(length(east.africa.grid) == length(human.pop.vec))
-  
-  # Generate pseudo-absences, only targeting grid cells with positive human 
-  # population densities to save computation time
+  # Generate pseudo-absences, only targeting grid cells with positive counts 
+  # to save computation time
   temp <- st_sample(
-    east.africa.grid[human.pop.vec > 0],
-    size = human.pop.vec[human.pop.vec > 0]
-  )
-  
-  temp <- temp %>%
+    east.africa.grid[counts > 0],
+    size = counts[counts > 0]
+  ) %>%
     st_intersection(., east.africa.lake.erase) %>%
     st_coordinates() %>%
     as.data.frame() %>%
-    # only pull a subset of points
-    sample_n(size = n.points.per.yrmon * 12) %>%
     rename(
       GPS_x = X,
       GPS_y = Y
+    )
+  
+  # Filter points to eliminate any within the outbreak buffer zone,
+  # assuming there are outbreak points in that year
+  if (length(outbreak.buffer) > 0) {
+    
+    temp <- st_difference(
+      st_as_sf(temp, coords = c("GPS_x", "GPS_y"), crs = st_crs(east.africa)),
+      outbreak.buffer
     ) %>%
+      mutate(
+        GPS_x = st_coordinates(.)[,1],
+        GPS_y = st_coordinates(.)[,2]
+      ) %>%
+      st_drop_geometry()
+  }
+  
+  assert_that(nrow(temp) >= n.points.per.yrmon * 12)
+  
+  # Subsample down to only the required number of points for a year
+  temp <- slice_sample(temp, n = n.points.per.yrmon * 12) %>%
     mutate(
       OB_Yr = rep(year, times = n.points.per.yrmon * 12),
       OB_Mo = rep(month.name, each = n.points.per.yrmon)
     )
   
-  human.points <- rbind(human.points, temp)  
+  human.points <- bind_rows(human.points, temp)
 }
 
 #==============================================================================
@@ -295,17 +290,12 @@ for(year in 2008:2022) {
 r <- terra::rast("data/rasters/healthcare/processed/healthcare_2.5min.tif")
 plot(r)
 
-# Aggregate the raster to 10 arcminute grid cells
-r.agg <- aggregate(r, fact = 4, fun = "mean")
-plot(r.agg)
-
-
 # Make the East Africa map into a grid matching the extent and resolution of 
 # the aggregated raster
 east.africa.grid <- st_make_grid(
   east.africa, 
-  n = c(dim(r.agg)[2], dim(r.agg)[1]),
-  cellsize = res(r.agg)
+  n = c(dim(r)[2], dim(r)[1]),
+  cellsize = res(r)
 )
 
 ggplot() + 
@@ -317,67 +307,75 @@ ggplot() +
 # Generate pseudo-absence points in inverse proportion to the 
 # healthcare travel time raster
 
-n.points.per.yrmon <- 150
 travel.points <- data.frame()
+
+set.seed(8)
 
 for(year in 2008:2022) {
   
-  # Modify the aggregated healthcare travel time raster to sample in the inverse
-  # and to increase the magnitude of the values, therefore generating a 
-  # reasonable number of pseudo-absence points
-  r.agg.mod <- (1/r.agg) * 100
+  # Generate a buffer representing RVF outbreak locations in the focal year
+  outbreak.buffer <- d.sf %>%
+    filter(OB_Yr == year) %>%
+    st_buffer(dist = outbreak.buffer.radius * 2) %>%
+    st_union()
   
-  # Generate a vector of cells for this year with RVF outbreak cases
-  outbreak.cells <- st_intersects(
-    filter(d.sf, OB_Yr == year),
-    east.africa.grid
-  ) %>%
-    unlist() %>%
-    unique() %>%
-    sort()
-  
-  print(outbreak.cells)
+  # Modify the healthcare travel time raster to sample in the inverse
+  r.mod <- 1 / r
   
   # Generate a vector of inverse travel time values
   # (corresponding to the value of each raster grid cell) 
-  travel.vec <- r.agg.mod %>%
+  travel.vec <- r.mod %>%
     # need to flip this raster vertically since "st_sample()" will start
     # sampling from the bottom left cell and move upwards rather than starting
     # from the upper left
     flip(direction = "vertical") %>%
     # extract values
     values() %>%
-    # convert population density values to integers
-    as.integer() %>%
     # convert NA values to zeroes
     ifelse(is.na(.), 0, .)
   
-  # Set outbreak cell values to 0 so they will not be sampled for 
-  # pseudo-absences
-  travel.vec[outbreak.cells] <- 0
+  # With what probability should each grid cell be sampled?
+  probs <- travel.vec / sum(travel.vec)
+  # Sample grid cells in proportion to their probability
+  samples <- sample(1:length(travel.vec), size = 10000, prob = probs, replace = TRUE)
+  # Generate a vector giving the point count needed from each grid cell
+  counts <- table(factor(samples, levels = 1:length(travel.vec)))
   
-  # How many pseudo-absences will be generated?
-  print(sum(travel.vec))
-  
-  assertthat::assert_that(length(east.africa.grid) == length(travel.vec))
+  assert_that(length(east.africa.grid) == length(counts))
   
   # Generate pseudo-absences, only targeting grid cells with positive travel 
   # values to save computation time
   temp <- st_sample(
-    east.africa.grid[travel.vec > 0],
-    size = travel.vec[travel.vec > 0]
-  )
-  
-  temp <- temp %>%
+    east.africa.grid[counts > 0],
+    size = counts[counts > 0]
+  ) %>%
     st_intersection(., east.africa.lake.erase) %>%
     st_coordinates() %>%
     as.data.frame() %>%
-    # only pull a subset of points
-    sample_n(size = n.points.per.yrmon * 12) %>%
     rename(
       GPS_x = X,
       GPS_y = Y
+    )
+  
+  # Filter points to eliminate any within the outbreak buffer zone,
+  # assuming there are outbreak points in that year
+  if (length(outbreak.buffer) > 0) {
+    
+    temp <- st_difference(
+      st_as_sf(temp, coords = c("GPS_x", "GPS_y"), crs = st_crs(east.africa)),
+      outbreak.buffer
     ) %>%
+      mutate(
+        GPS_x = st_coordinates(.)[,1],
+        GPS_y = st_coordinates(.)[,2]
+      ) %>%
+      st_drop_geometry()
+  }
+  
+  assert_that(nrow(temp) >= n.points.per.yrmon * 12)
+  
+  # Subsample down to only the required number of points for a year
+  temp <- slice_sample(temp, n = n.points.per.yrmon * 12) %>%
     mutate(
       OB_Yr = rep(year, times = n.points.per.yrmon * 12),
       OB_Mo = rep(month.name, each = n.points.per.yrmon)
@@ -401,8 +399,18 @@ d.random.sf <- d.random %>%
   
 ggplot() +
   geom_sf(data = east.africa, fill = "white") +
-  geom_sf(data = lakes.10, fill = "lightblue") +
+  geom_sf(data = lakes.5, fill = "lightblue") +
   geom_sf(data = d.random.sf, aes(color = as.factor(RVF_presence))) +
+  scale_color_manual(values = c(alpha("gray", 0.2), "darkred")) +
+  theme_void() +
+  theme(
+    legend.position = "none"
+  )
+
+ggplot() +
+  geom_sf(data = east.africa, fill = "white") +
+  geom_sf(data = lakes.5, fill = "lightblue") +
+  geom_sf(data = filter(d.random.sf, OB_Yr == 2018), aes(color = as.factor(RVF_presence))) +
   scale_color_manual(values = c(alpha("gray", 0.2), "darkred")) +
   theme_void() +
   theme(
@@ -426,8 +434,18 @@ d.doughnut.sf <- d.doughnut %>%
 
 ggplot() +
   geom_sf(data = east.africa, fill = "white") +
-  geom_sf(data = lakes.10, fill = "lightblue") +
+  geom_sf(data = lakes.5, fill = "lightblue") +
   geom_sf(data = d.doughnut.sf, aes(color = as.factor(RVF_presence))) +
+  scale_color_manual(values = c(alpha("gray", 0.2), "darkred")) +
+  theme_void() +
+  theme(
+    legend.position = "none"
+  )
+
+ggplot() +
+  geom_sf(data = east.africa, fill = "white") +
+  geom_sf(data = lakes.5, fill = "lightblue") +
+  geom_sf(data = filter(d.doughnut.sf, OB_Yr == 2018), aes(color = as.factor(RVF_presence))) +
   scale_color_manual(values = c(alpha("gray", 0.2), "darkred")) +
   theme_void() +
   theme(
@@ -451,8 +469,18 @@ d.popweighted.sf <- d.popweighted %>%
 
 ggplot() +
   geom_sf(data = east.africa.lake.erase, fill = "white") +
-  geom_sf(data = lakes.10, fill = "lightblue") +
+  geom_sf(data = lakes.5, fill = "lightblue") +
   geom_sf(data = d.popweighted.sf, aes(color = as.factor(RVF_presence))) +
+  scale_color_manual(values = c(alpha("gray", 0.2), "darkred")) +
+  theme_void() +
+  theme(
+    legend.position = "none"
+  )
+
+ggplot() +
+  geom_sf(data = east.africa.lake.erase, fill = "white") +
+  geom_sf(data = lakes.5, fill = "lightblue") +
+  geom_sf(data = filter(d.popweighted.sf, OB_Yr == 2018), aes(color = as.factor(RVF_presence))) +
   scale_color_manual(values = c(alpha("gray", 0.2), "darkred")) +
   theme_void() +
   theme(
@@ -476,8 +504,18 @@ d.travel.sf <- d.travel %>%
 
 ggplot() +
   geom_sf(data = east.africa, fill = "white") +
-  geom_sf(data = lakes.10, fill = "lightblue") +
+  geom_sf(data = lakes.5, fill = "lightblue") +
   geom_sf(data = d.travel.sf, aes(color = as.factor(RVF_presence))) +
+  scale_color_manual(values = c(alpha("gray", 0.2), "darkred")) +
+  theme_void() +
+  theme(
+    legend.position = "none"
+  )
+
+ggplot() +
+  geom_sf(data = east.africa, fill = "white") +
+  geom_sf(data = lakes.5, fill = "lightblue") +
+  geom_sf(data = filter(d.travel.sf, OB_Yr == 2018), aes(color = as.factor(RVF_presence))) +
   scale_color_manual(values = c(alpha("gray", 0.2), "darkred")) +
   theme_void() +
   theme(
@@ -490,6 +528,8 @@ ggplot() +
   scale_color_manual(values = c(alpha("gray", 0.2), "darkred")) +
   theme_void() +
   facet_wrap(~OB_Yr)
+
+#==============================================================================
 
 
 # Save data
