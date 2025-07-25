43dd4083ef1122b502fa499f4ba786dd2e84f319
Evan Eskew <eveskew@gmail.com>
2025-07-25 10:43:44 -0400

    Updating new outbreak/pseudoabsence data with predictors

--- scripts/extract_outbreak_absence_predictors.R
+++ scripts/extract_outbreak_absence_predictors.R
@@ -29,15 +29,13 @@ monthly.predictors <- read_csv("data/predictor_flat_files/monthly_predictors_his
 d.random <- read_csv("data/outbreak_data/outbreak_data_w_random_pseudoabsences.csv") %>%
   rename(
     year = outbreak_year,
-    month = outbreak_month,
-    x = GPS_x,
-    y = GPS_y
+    month = outbreak_month
   )
 
 d.random$grid_cell <- cellFromXY(
   r,
   d.random %>%
-    select(x, y) %>%
+    select(GPS_x, GPS_y) %>%
     as.matrix()
 )
 
@@ -50,15 +48,13 @@ d.random <- d.random %>%
 d.popweighted <- read_csv("data/outbreak_data/outbreak_data_w_popweighted_pseudoabsences.csv") %>%
   rename(
     year = outbreak_year,
-    month = outbreak_month,
-    x = GPS_x,
-    y = GPS_y
+    month = outbreak_month
   )
 
 d.popweighted$grid_cell <- cellFromXY(
   r,
   d.popweighted %>%
-    select(x, y) %>%
+    select(GPS_x, GPS_y) %>%
     as.matrix()
 )
 
@@ -67,84 +63,88 @@ d.popweighted <- d.popweighted %>%
   left_join(., yearly.predictors, by = c("grid_cell", "year")) %>%
   left_join(., monthly.predictors, by = c("grid_cell", "year", "month"))
 
+
+d.travel <- read_csv("data/outbreak_data/outbreak_data_w_travel_pseudoabsences.csv") %>%
+  rename(
+    year = outbreak_year,
+    month = outbreak_month
+  )
+
+d.travel$grid_cell <- cellFromXY(
+  r,
+  d.travel %>%
+    select(GPS_x, GPS_y) %>%
+    as.matrix()
+)
+
+d.travel <- d.travel %>%
+  left_join(., static.predictors, by = "grid_cell") %>%
+  left_join(., yearly.predictors, by = c("grid_cell", "year")) %>%
+  left_join(., monthly.predictors, by = c("grid_cell", "year", "month"))
+
 #==============================================================================
 
 
-ggplot() +
-  geom_jitter(data = d.random, aes(x = RVF_presence, y = elevation))
-
-ggplot() +
-  geom_jitter(data = d.popweighted, aes(x = RVF_presence, y = elevation))
-
-
-ggplot() +
-  geom_jitter(data = d.random, aes(x = RVF_presence, y = slope))
-
-ggplot() +
-  geom_jitter(data = d.popweighted, aes(x = RVF_presence, y = slope))
+d.all <- bind_rows(
+  mutate(d.random, group = rep("random", nrow(d.random))),
+  mutate(d.popweighted, group = rep("popweighted", nrow(d.popweighted))),
+  mutate(d.travel, group = rep("travel", nrow(d.travel)))
+)
 
 
 ggplot() +
-  geom_jitter(data = d.random, aes(x = RVF_presence, y = travel_time_to_healthcare))
-
-ggplot() +
-  geom_jitter(data = d.popweighted, aes(x = RVF_presence, y = travel_time_to_healthcare))
+  geom_jitter(data = d.all, aes(x = RVF_presence, y = elevation), color = alpha("black", 0.2)) +
+  facet_wrap(~group)
 
 
 ggplot() +
-  geom_jitter(data = d.random, aes(x = RVF_presence, y = human_pop))
-
-ggplot() +
-  geom_jitter(data = d.popweighted, aes(x = RVF_presence, y = human_pop))
+  geom_jitter(data = d.all, aes(x = RVF_presence, y = slope), color = alpha("black", 0.2)) +
+  facet_wrap(~group)
 
 
 ggplot() +
-  geom_jitter(data = d.random, aes(x = RVF_presence, y = dist_to_lake_all))
-
-ggplot() +
-  geom_jitter(data = d.popweighted, aes(x = RVF_presence, y = dist_to_lake_all))
+  geom_jitter(data = d.all, aes(x = RVF_presence, y = travel_time_to_healthcare), color = alpha("black", 0.2)) +
+  facet_wrap(~group)
 
 
 ggplot() +
-  geom_jitter(data = d.random, aes(x = RVF_presence, y = dist_to_river_10))
-
-ggplot() +
-  geom_jitter(data = d.popweighted, aes(x = RVF_presence, y = dist_to_river_10))
+  geom_jitter(data = d.all, aes(x = RVF_presence, y = human_pop), color = alpha("black", 0.2)) +
+  facet_wrap(~group)
 
 
 ggplot() +
-  geom_jitter(data = d.random, aes(x = RVF_presence, y = cattle_density))
-
-ggplot() +
-  geom_jitter(data = d.popweighted, aes(x = RVF_presence, y = cattle_density))
+  geom_jitter(data = d.all, aes(x = RVF_presence, y = dist_to_lake_all), color = alpha("black", 0.2)) +
+  facet_wrap(~group)
 
 
 ggplot() +
-  geom_jitter(data = d.random, aes(x = RVF_presence, y = goat_density))
-
-ggplot() +
-  geom_jitter(data = d.popweighted, aes(x = RVF_presence, y = goat_density))
+  geom_jitter(data = d.all, aes(x = RVF_presence, y = dist_to_river_10), color = alpha("black", 0.2)) +
+  facet_wrap(~group)
 
 
 ggplot() +
-  geom_jitter(data = d.random, aes(x = RVF_presence, y = sheep_density))
-
-ggplot() +
-  geom_jitter(data = d.popweighted, aes(x = RVF_presence, y = sheep_density))
+  geom_jitter(data = d.all, aes(x = RVF_presence, y = cattle_density), color = alpha("black", 0.2)) +
+  facet_wrap(~group)
 
 
 ggplot() +
-  geom_jitter(data = d.random, aes(x = RVF_presence, y = monthly_precip))
-
-ggplot() +
-  geom_jitter(data = d.popweighted, aes(x = RVF_presence, y = monthly_precip))
+  geom_jitter(data = d.all, aes(x = RVF_presence, y = goat_density), color = alpha("black", 0.2)) +
+  facet_wrap(~group)
 
 
 ggplot() +
-  geom_jitter(data = d.random, aes(x = RVF_presence, y = monthly_tmax))
+  geom_jitter(data = d.all, aes(x = RVF_presence, y = sheep_density), color = alpha("black", 0.2)) +
+  facet_wrap(~group)
+
 
 ggplot() +
-  geom_jitter(data = d.popweighted, aes(x = RVF_presence, y = monthly_tmax))
+  geom_jitter(data = d.all, aes(x = RVF_presence, y = monthly_precip), color = alpha("black", 0.2)) +
+  facet_wrap(~group)
+
+
+ggplot() +
+  geom_jitter(data = d.all, aes(x = RVF_presence, y = monthly_tmax), color = alpha("black", 0.2)) +
+  facet_wrap(~group)
 
 #==============================================================================
 
@@ -157,8 +157,8 @@ d.random %>%
   select(!all_of(cols.to.exclude)) %>%
   mutate(month_numeric = match(month, month.name)) %>%
   rename(
-    longitude = x,
-    latitude = y
+    longitude = GPS_x,
+    latitude = GPS_y
   ) %>%
   write_csv(file = "data/outbreak_data/outbreak_data_w_random_pseudoabsences_predictors.csv")
 
@@ -166,7 +166,16 @@ d.popweighted %>%
   select(!all_of(cols.to.exclude)) %>%
   mutate(month_numeric = match(month, month.name)) %>%
   rename(
-    longitude = x,
-    latitude = y
+    longitude = GPS_x,
+    latitude = GPS_y
   ) %>%
   write_csv(file = "data/outbreak_data/outbreak_data_w_popweighted_pseudoabsences_predictors.csv")
+
+d.travel %>%
+  select(!all_of(cols.to.exclude)) %>%
+  mutate(month_numeric = match(month, month.name)) %>%
+  rename(
+    longitude = GPS_x,
+    latitude = GPS_y
+  ) %>%
+  write_csv(file = "data/outbreak_data/outbreak_data_w_travel_pseudoabsences_predictors.csv")
